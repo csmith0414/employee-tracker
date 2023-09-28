779da3fe11ee897429f6ad9ba572dbdf76a30aab
Chad Smith <cds021@yahoo.com>
2023-09-28 10:56:54 -0400

    updating schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,20 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
+    PRIMARY KEY (id),
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    
 )
 
 CREATE TABLE employee (
@@ -22,6 +25,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
     REFERENCES role(id),
     FOREIGN KEY (manager_id)
